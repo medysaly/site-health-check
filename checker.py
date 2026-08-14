bfeaba84095e6035b326954bbea151b73316ca19
Mehdi Salhi <mehdisalhi.dev@gmail.com>
2026-08-14 12:33:15 -0400

    create container and slack webhook

--- checker.py
+++ checker.py
@@ -5,17 +5,21 @@ from datetime import datetime
 with open("check_log.txt", "a") as log_file:
     
 
-    site = "https://www.mehdisalhi.com"
+    sites = ["https://www.mehdisalhi.com", "https://www.unkommon.ai", "https://www.beesknees.ai/"]
 
-    try:
-        response = requests.get(site)
+    for site in sites:
 
-        response_time = response.elapsed.total_seconds()
 
-        if response.status_code == 200:
-            log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} : The site {site} is up and running. Response time: {round(response_time, 2)} seconds.\n")
-        else:
-            log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} : The site {site} is down. Status code: {response.status_code}\n")
+        try:
 
-    except requests.exceptions.RequestException as e:
-        log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} : The site {site} is down. Error: {e}\n")
+            response = requests.get(site, timeout=5)
+
+            response_time = response.elapsed.total_seconds()
+
+            if response.status_code == 200:
+                log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} : The site {site} is up and running. Response time: {round(response_time, 2)} seconds.\n")
+            else:
+                log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} : The site {site} is down. Status code: {response.status_code}\n")
+
+        except requests.exceptions.RequestException as e:
+            log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} : The site {site} is down. Error: {e}\n")
