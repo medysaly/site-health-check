545ca55acb48af967d8e90d7e0fc1f6ef7d800d5
Mehdi Salhi <mehdisalhi.dev@gmail.com>
2026-08-14 13:55:49 -0400

    added dockerignore/compose.yaml

--- checker.py
+++ checker.py
@@ -1,11 +1,21 @@
 import requests
 from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+slack_webhook = os.getenv("SLACK_WEBHOOK_URL")
+
+def send_alert(message):
+    send = requests.post(slack_webhook, json={"text": message})
+
 
 
 with open("check_log.txt", "a") as log_file:
     
 
-    sites = ["https://www.mehdisalhi.com", "https://www.unkommon.ai", "https://www.beesknees.ai/"]
+    sites = ["https://www.mehdisalhi.com", "https://www.unkommon.ai", "https://www.beesknees.ai/", "www.examplem.com"]
 
     for site in sites:
 
@@ -20,6 +30,8 @@ with open("check_log.txt", "a") as log_file:
                 log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} : The site {site} is up and running. Response time: {round(response_time, 2)} seconds.\n")
             else:
                 log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} : The site {site} is down. Status code: {response.status_code}\n")
+                send_alert(f"The site {site} is down. Status code: {response.status_code}")
 
         except requests.exceptions.RequestException as e:
             log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} : The site {site} is down. Error: {e}\n")
+            send_alert(f"The site {site} is down. Error: {e}")
